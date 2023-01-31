2b566763c360a8663e30a2fbd6d3ada15d533f5b
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-01-31 13:26:12 +0600

    :memo: Add new Handlers

---
+++ cmd/api/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createItemHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showItemHandler)
+
+	return router
+}
