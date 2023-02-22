ce4a0f2f3c56be509163be2538f281bb4ac04065
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-02-22 21:16:54 +0600

    Add custom logger

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,10 +5,6 @@ import (
 	"net/http"
 )
 
-func (app *application) logError(r *http.Request, err error) {
-	app.logger.Println(err)
-}
-
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
 	env := envelope{"error": message}
 
@@ -41,4 +37,13 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+func (app *application) logError(r *http.Request, err error) {
+	// Use the PrintError() method to log the error message, and include the current
+	// request method and URL as properties in the log entry.
+	app.logger.PrintError(err, map[string]string{
+		"request_method": r.Method,
+		"request_url":    r.URL.String(),
+	})
 }
